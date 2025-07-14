758022e6288e3db9cae5c35437598209a444cc9d
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-07-15 00:24:43 +0300

    Modified

--- client/app/components/Layout/Header/Header.tsx
+++ client/app/components/Layout/Header/Header.tsx
@@ -6,70 +6,68 @@ import { ProfileImage } from "../../UI/ProfileImage/ProfileImage";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.box}>
-          {/* <Image src="/icons/menu.svg" alt="Menu Icon" width={28} height={28} /> */}
-          <Link href="/" className={styles.logo}>
+      <div className={styles.box}>
+        {/* <Image src="/icons/menu.svg" alt="Menu Icon" width={28} height={28} /> */}
+        <Link href="/" className={styles.logo}>
+          <Image
+            src="/images/test-logo.webp"
+            alt="Logo Icon"
+            width={90}
+            height={55}
+            draggable={false}
+            priority
+          />
+        </Link>
+      </div>
+      <div className={styles.box}>
+        <div className={styles.links}>
+          <Link href="/" className={styles.link}>
             <Image
-              src="/images/test-logo.webp"
-              alt="Logo Icon"
-              width={90}
-              height={55}
+              src="/icons/about.svg"
+              alt="About Icon"
+              width={25}
+              height={25}
               draggable={false}
-              priority
             />
+            Despre noi
+          </Link>
+          <Link href="/search" className={styles.link}>
+            <Image
+              src="/icons/search-yellow.svg"
+              alt="Yellow Search Icon"
+              width={19}
+              height={19}
+              draggable={false}
+            />
+            Caută anunțuri
+          </Link>
+          <Link href="/search" className={styles.link}>
+            <Image
+              src="/icons/yellow-heart.svg"
+              alt="Yellow Heart Icon"
+              width={20}
+              height={20}
+              draggable={false}
+            />
+            Cazuri fericite
+          </Link>
+          <Link href="/search" className={styles.link}>
+            <Image
+              src="/badges/crew-badge.webp"
+              alt="Lost & Found Crew Badge"
+              width={30}
+              height={30}
+              draggable={false}
+            />
+            Lost & Found Crew
           </Link>
         </div>
-        <div className={styles.box}>
-          <div className={styles.links}>
-            <Link href="/" className={styles.link}>
-              <Image
-                src="/icons/about.svg"
-                alt="About Icon"
-                width={25}
-                height={25}
-                draggable={false}
-              />
-              Despre noi
-            </Link>
-            <Link href="/search" className={styles.link}>
-              <Image
-                src="/icons/search-yellow.svg"
-                alt="Yellow Search Icon"
-                width={19}
-                height={19}
-                draggable={false}
-              />
-              Caută anunțuri
-            </Link>
-            <Link href="/search" className={styles.link}>
-              <Image
-                src="/icons/yellow-heart.svg"
-                alt="Yellow Heart Icon"
-                width={20}
-                height={20}
-                draggable={false}
-              />
-              Cazuri fericite
-            </Link>
-            <Link href="/search" className={styles.link}>
-              <Image
-                src="/badges/crew-badge.webp"
-                alt="Lost & Found Crew Badge"
-                width={30}
-                height={30}
-                draggable={false}
-              />
-              Lost & Found Crew
-            </Link>
-          </div>
-          <Link href="/create-post">
-            <button className={styles.button}>
-              <span>+ </span> Postează
-            </button>
-          </Link>
-          <ProfileImage />
-        </div>
+        <Link href="/create-post">
+          <button className={styles.button}>
+            <span>+ </span> Postează
+          </button>
+        </Link>
+        <ProfileImage />
       </div>
     </header>
   );
